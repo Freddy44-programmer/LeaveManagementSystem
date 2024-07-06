e91dc02715531b1dc0ffb98bb1783ab77156f6a6
Thabang <Thabang@DESKTOP-867BMT0>
2024-07-06 18:44:35 +0200

    create and Index modified

---
+++ LeaveManagementSystem/Data/LeaveType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementSystem.Data
+{
+    public class LeaveType
+    {
+    
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public int NumberOfDays { get; set; }
+
+    }
+}
